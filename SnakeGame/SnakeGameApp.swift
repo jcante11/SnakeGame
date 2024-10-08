f443b5e1c48cf3c9aad0e6ba2d1396911a52f8ed
jcante11 <juan_cante@uri.edu>
2024-10-08 16:34:25 -0400

    Initial Commit

---
+++ SnakeGame/SnakeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameApp.swift
+//  SnakeGame
+//
+//  Created by Juan Cante Jr. on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
